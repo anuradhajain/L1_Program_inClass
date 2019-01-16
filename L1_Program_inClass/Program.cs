1857b9ff1e87084aa1bc6335351b5873881ac180
anuradha <anuradha@SID-PC>
2019-01-15 21:17:06 -0500

    Version  2 of L1_Program_inClass

--- L1_Program_inClass/Program.cs
+++ L1_Program_inClass/Program.cs
@@ -6,7 +6,35 @@ namespace L1_Program_inClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Please enter a number:");
+                string input = Console.ReadLine();
+
+                int number = int.Parse(input);
+
+                int modules = number % 2;
+
+                if (modules == 0)
+                {
+                    Console.WriteLine("Its an even number:");
+                } // end of if
+
+                else
+                {
+                    Console.WriteLine("Its an odd number");
+                } // end of else
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+            } // end of try
+
+            catch
+            {
+                Console.WriteLine("There is some error. Please enter a valid integer next time");
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey(true);
+            } // end of catch
         }
     }
 }
